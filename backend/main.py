93b3f3142b29fc2db4e75a76ef5887519c381276
Erica Han <ericahan.38@gmail.com>
2025-07-09 01:01:12 -0400

    CORS setup

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models, database, routes
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 app.include_router(routes.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://flora-steel-three.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
