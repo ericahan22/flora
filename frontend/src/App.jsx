fd31c362557be6ef7d7be252d925e3b23f23ca8c
Erica Han <ericahan.38@gmail.com>
2025-07-09 00:52:40 -0400

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,35 +3,44 @@ import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+// Folium-like marker icon
+const greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+})
+
 function App() {
   const [speciesData, setSpeciesData] = useState([])
 
-  useEffect(() => {    // ??? 
-    fetch('http://flora-steel-three.vercel.app/')
+  useEffect(() => { 
+    fetch('https://flora-8f34.onrender.com/api/observations`/')
     .then(res => res.json())
     .then(data => setSpeciesData(data))
   }, [])
 
   return (
-    <div>
-      <h1>Observed Species</h1>
-      <MapContainer center={[43.47, -80.53]} zoom={12} style={{ height: '600px', width: '100%'}}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {speciesData.map((obs, idx) =>
-          obs.latitude && obs.longitude ? (
-            <Marker key={idx} position={[obs.latitude, obs.longitude]}>
-              <Popup>
+    <MapContainer center={[43.47, -80.53]} zoom={12} style={{ height: '100vh', width: '100vw' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {speciesData.map((obs, idx) =>
+        obs.latitude && obs.longitude ? (
+          <Marker key={idx} position={[obs.latitude, obs.longitude]} icon={greenIcon}>
+            <Popup>
+              <div style={{ minWidth: 180 }}>
                 <b>Scientific Name:</b> {obs.scientific_name}<br />
                 <b>Common Name:</b> {obs.name || "N/A"}<br />
                 <b>Date:</b> {obs.date || "N/A"}
-              </Popup>
-            </Marker>
-          ) : null
-        )}
-      </MapContainer>
-    </div>
+              </div>
+            </Popup>
+          </Marker>
+        ) : null
+      )}
+    </MapContainer>
   )
 }
 
