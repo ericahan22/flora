037c15e26ee4a16fd42e7936e43be2582f882cfe
Erica Han <ericahan.38@gmail.com>
2025-07-09 01:35:21 -0400

    switch to session pooler

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=0
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
