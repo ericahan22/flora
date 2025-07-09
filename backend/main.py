7fa146f13ad5c0a0a35b7c469ac9c4ff24e7e32f
Erica Han <ericahan.38@gmail.com>
2025-07-09 02:42:52 -0400

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,13 @@ from . import models, database, routes
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
-app.include_router(routes.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://flora-steel-three.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://flora-steel-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+app.include_router(routes.router)
